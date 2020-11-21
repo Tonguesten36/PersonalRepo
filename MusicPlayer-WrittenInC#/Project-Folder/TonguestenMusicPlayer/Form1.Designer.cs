bfc10a8033ecfc873157bf00667237811adaccb4
TungNotTongue <tung.gumball@gmail.com>
2020-11-21 22:34:53 +0700

    Added the Music Player to the repository

---
+++ MusicPlayer-WrittenInC#/Project-Folder/TonguestenMusicPlayer/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TheClickMeButton = new System.Windows.Forms.Button();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.Label = new System.Windows.Forms.Label();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.musicStatusIndicator = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TheClickMeButton
+            // 
+            this.TheClickMeButton.Location = new System.Drawing.Point(0, 0);
+            this.TheClickMeButton.Name = "TheClickMeButton";
+            this.TheClickMeButton.Size = new System.Drawing.Size(75, 23);
+            this.TheClickMeButton.TabIndex = 0;
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(0, 0);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(100, 20);
+            this.InputBox.TabIndex = 0;
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayButton.Location = new System.Drawing.Point(230, 162);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(92, 38);
+            this.PlayButton.TabIndex = 2;
+            this.PlayButton.Text = "Play";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PauseButton.Location = new System.Drawing.Point(331, 163);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(92, 38);
+            this.PauseButton.TabIndex = 3;
+            this.PauseButton.Text = "Stop";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Font = new System.Drawing.Font("Impact", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Label.Location = new System.Drawing.Point(77, 9);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(384, 48);
+            this.Label.TabIndex = 4;
+            this.Label.Text = "Tongue\'s Music Player";
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrowseButton.Location = new System.Drawing.Point(128, 162);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(92, 38);
+            this.BrowseButton.TabIndex = 5;
+            this.BrowseButton.Text = "Browse...";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseFile_Click);
+            // 
+            // musicStatusIndicator
+            // 
+            this.musicStatusIndicator.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.musicStatusIndicator.Location = new System.Drawing.Point(63, 57);
+            this.musicStatusIndicator.Name = "musicStatusIndicator";
+            this.musicStatusIndicator.Size = new System.Drawing.Size(422, 63);
+            this.musicStatusIndicator.TabIndex = 6;
+            this.musicStatusIndicator.Text = "You\'re not playing any songs yet...\r\n";
+            this.musicStatusIndicator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(63, 123);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(422, 20);
+            this.filePath.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(190, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 43);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "About this Program";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.AboutProgram_Click);
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(542, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.musicStatusIndicator);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.PlayButton);
+            this.Name = "MainForm";
+            this.Text = "Munsic Playaarr";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TheClickMeButton;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Label musicStatusIndicator;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MusicPlayer-WrittenInC#/Project-Folder/TonguestenMusicPlayer/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using System.Media;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class MainForm : Form
+    {
+        MusicPlayer musicPlayer;
+        OpenFileDialog openFileDialog;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            musicPlayer = new MusicPlayer();
+            MusicPlayerStatus();
+        }
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            musicPlayer.PlayMusic();
+            MusicPlayerStatus();
+        }
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            musicPlayer.PauseMusic();
+            MusicPlayerStatus();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void BrowseFile_Click(object sender, EventArgs e)
+        {
+            musicPlayer.BrowseMusic(openFileDialog, filePath);
+            MusicPlayerStatus();
+        }
+
+        private void AboutProgram_Click(object sender, EventArgs e)
+        {
+            Form aboutForm = new Form();
+            musicPlayer.ShowAboutProgram(aboutForm);
+        }
+
+        private void MusicPlayerStatus()
+        {
+            musicPlayer.DisplayMusicPlayerStatus(musicStatusIndicator);
+        }
+    }
+}
