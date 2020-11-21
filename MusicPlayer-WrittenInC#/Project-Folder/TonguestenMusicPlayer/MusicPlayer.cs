0eb38f1d8a3640292b19adf1d9b63ddaef10af6f
TungNotTongue <tung.gumball@gmail.com>
2020-11-21 22:39:28 +0700

    21/11/2020

---
+++ MusicPlayer-WrittenInC#/Project-Folder/TonguestenMusicPlayer/MusicPlayer.cs
@@ -0,0 +1,79 @@
+﻿using System.Media;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    class MusicPlayer
+    {
+        SoundPlayer soundPlayer;
+
+        bool hasMusicLoaded;
+        bool isPlayingMusic;
+
+        public MusicPlayer()
+        {
+            soundPlayer = new SoundPlayer();
+            hasMusicLoaded = false;
+            isPlayingMusic = false;
+        }
+        public void PlayMusic()
+        {
+            soundPlayer.Play();
+            isPlayingMusic = true;
+        }
+        public void PauseMusic()
+        {
+            soundPlayer.Stop();
+            isPlayingMusic = false;
+        }
+        public void LoadMusic()
+        {
+            soundPlayer.Load();
+            hasMusicLoaded = true;
+        }
+        public void BrowseMusic(OpenFileDialog openFileDialog, TextBox filePathTextBox)
+        {
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = @"c:\Users\Personal\Music";
+
+            openFileDialog.CheckFileExists = true;
+
+            openFileDialog.Filter = "WAV files (*.wav)|*.wav";
+            openFileDialog.DefaultExt = ".wav";
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePathTextBox.Text = openFileDialog.FileName;
+                soundPlayer.SoundLocation = filePathTextBox.Text;
+                hasMusicLoaded = true;
+            }
+        }
+        public void ShowAboutProgram(Form aboutForm)
+        {
+            if(aboutForm != null)
+            {
+                string aboutText = "This music player was developed by Tonguesten\nMade with Visual Studio 2019 - Written in C#";
+                MessageBox.Show(aboutText);
+            }
+        }
+        public void DisplayMusicPlayerStatus(Label musicStatusLabel)
+        {
+            if(!hasMusicLoaded)
+            {
+                musicStatusLabel.Text = "There is no song loaded. Click 'Browse' to load a song";
+            }
+            else
+            {
+                if (!isPlayingMusic)
+                {
+                    musicStatusLabel.Text = "Currently not playing music.";
+                }
+                else
+                {
+                    musicStatusLabel.Text = "Now playing music.";
+                }
+            }
+
+        }
+    }
+}
