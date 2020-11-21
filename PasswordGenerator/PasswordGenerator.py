7bd29e606dfeb86ae6675f3a46856f48af56f1ca
TungNotTongue <tung.gumball@gmail.com>
2020-11-21 16:36:03 +0700

    Added the Pomodoro Clock program (along with the audio file used for the project)

--- PasswordGenerator/PasswordGenerator.py
+++ PasswordGenerator/PasswordGenerator.py
@@ -22,45 +22,67 @@ def password_generator(length):
     return print("".join(password))
 
 
+def main():
+    print("##############################################################")
+    print("\n---------- Password Generator ----------")
+
+    request = input("------ Do you want to generate a new password? [Y/N] ")
+
+    # check validity of the request
+    try:
+        str(request)
+        # if the user said Yes
+        if request == "Y" or request == "y":
+            length = input("How long do you want your password to be? ")
+            try:
+                # prompt the user for password's length
+                int(length)
+
+                # prompt the user for intended password's feature (still work-in-progress)
+                # uppercase_request = str(input("Uppercase letters in your password? [Y/N] "))  # do they want uppercase letter?
+                # number_request = str(input("Numbers? [Y/N] "))  # numbers?
+                # symbol_request = str(input("Symbols [Y/N] "))  # symbols?
+
+                print("Generating password...")
+                sleep(1)
+
+                # generating password
+                print("Password: \b")
+                password_generator(length)
+            # if the user does not return a specific length
+            except ValueError:
+                print("We need a specific number")
+        else:
+            # if the user said No
+            if request == "N" or request == "n":
+                print("Going back...")
+                sleep(3)
+            else:
+                print("Please return an appropriate command.")
+    except ValueError:
+        print("An unknown error occur.")
+
+
+def about():
+    print("##############################################################")
+    print("----------| Password Generator is a program that generates a randomised password |----------")
+    print("----------| Made by Tonguesten |----------")
+
+
 if __name__ == '__main__':
     while True:
-        print("\n---------- Password Generator ----------")
+        print("##############################################################")
+        print("------| Select the following operations |------")
+        print("1. Password Generator \n2. About this software \n3. Exit the program")
+        opt = input("Enter here: ")
 
-        # check validity of the request
-        try:
-            request = input("------ Do you want to generate a new password? [Y/N] ")
-            str(request)
-            # if the user said Yes
-            if request == "Y" or request == "y":
-                try:
-                    # prompt the user for password's length
-                    length = int(input("How long do you want your password to be? "))
-                    int(length)
-
-                    # prompt the user for intended password's feature (still work-in-progress)
-                    # uppercase_request = str(input("Uppercase letters in your password? [Y/N] "))  # do they want uppercase letter?
-                    # number_request = str(input("Numbers? [Y/N] "))  # numbers?
-                    # symbol_request = str(input("Symbols [Y/N] "))  # symbols?
-
-                    print("Generating password...")
-                    sleep(1)
-
-                    # generating password
-                    print("Password: \b")
-                    password_generator(length)
-                # if the user does not return a specific length
-                except ValueError:
-                    print("We need a specific number")
-            else:
-                # if the user said No
-                if request == "N" or request == "n":
-                    print("Thanks for using this project.")
-                    for i in range(0, 3, 1):
-                        print("...")
-                        sleep(0.5)
-                    sleep(0.5)
-                    exit()
-                else:
-                    print("Please return an appropriate command.")
-        except ValueError:
-            print("An unknown error occur.")
+        if opt == "1":
+            main()
+        elif opt == "2":
+            about()
+        elif opt == "3":
+            print("Thank you for using this program...")
+            sleep(3)
+            exit()
+        else:
+            print("No such operation exists")
---
+++ PomodoroClock/ProgramOverview.md
@@ -0,0 +1,2 @@
+Hello World!
+This program is basically an utility that applies the Pomodoro technique to help you concentrate into your task.
