dd5b605b03f81a05f08f2aca408a76623083faa1
TungNotTongue <tung.gumball@gmail.com>
2020-11-25 10:07:36 +0700

    So anyway, I decided to create a password manager that will manage the different passwords that you use on different websites, and then I'll group the manager with the password generator, along with a possible password strength checker, into one program called Password Utility.

---
+++ TonguestenPassword/PasswordUtility.py
@@ -0,0 +1,23 @@
+from time import sleep
+from TonguestenPassword import PasswordGenerator
+
+if __name__ == '__main__':
+    while True:
+        print("################ PASSWORD UTILITY ######################")
+        print("1. Password Generator\n2. Password Manager\n3. About the Password-Related Utility\n4. Exit the utility")
+        operation = input("Choose an operation (enter an integer): ")
+
+        if operation == "1":
+            PasswordGenerator.main_program()
+        elif operation == "2":
+            print("################ PASSWORD MANAGER ######################")
+        elif operation == "3":
+            print("################ ABOUT THIS PROGRAM ######################")
+            print("---------| This utility was developed by Tonguesten in 2020 |---------")
+            print("---------| This utility was written in Python (3.9) |---------")
+        elif operation == "4":
+            print("Thank you for using Password Utility...")
+            sleep(3)
+            exit()
+        elif operation == "n":
+            print("bye")
